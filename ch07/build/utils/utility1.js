c3f7d796dc236556a7c11cb07609982f5a8b64eb
RATSENO <onestar1014@gmail.com>
2018-12-25 00:54:23 +0900

    07-17 모듈
    모듈이란 독립성을 가진 재사용 가능한 코드 블록이다.
    
    코드 블록 안에서 import, export 구문을 이용해서 모듈을 가져오거나
    내보낼 수 있다.

---
+++ ch07/build/utils/utility1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.add = add;
+var var1 = exports.var1 = 1000;
+function add(a, b) {
+    return a + b;
+}
+
+//let var1 = 1000;
+//function add(a, b){
+//    return a + b; 
+//}
+//export { var1 , add};
