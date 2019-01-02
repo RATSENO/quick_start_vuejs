5c3aa0592991028281a537c8c62e726e2b455ad2
RATSENO <onestar1014@gmail.com>
2019-01-02 17:36:00 +0900

    [MOD]소스 정리

--- ch10/contactapp/src/App.vue
+++ ch10/contactapp/src/App.vue
@@ -38,9 +38,18 @@ export default {
         return { 
             currentView : null, 
             //mode : 'add',
-            contact : { no:0, name:'', tel:'', address:'', photo:'' },
+            contact : { 
+                no:0, 
+                name:'', 
+                tel:'', 
+                address:'', 
+                photo:'' 
+            },
             contactlist : {
-                pageno:1, pagesize: CONF.PAGESIZE, totalcount:0, contacts:[]
+                pageno:1, 
+                pagesize: CONF.PAGESIZE, 
+                totalcount:0, 
+                contacts:[]
             }
         }
     },
