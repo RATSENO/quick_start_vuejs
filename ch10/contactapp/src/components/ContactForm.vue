d528df45afb6dc976a03d1443a458b3560b094f6
RATSENO <onestar1014@gmail.com>
2019-01-02 15:32:36 +0900

    [MOD]오타 수정

--- ch10/contactapp/src/components/ContactForm.vue
+++ ch10/contactapp/src/components/ContactForm.vue
@@ -8,15 +8,15 @@
             </div>
             <div class="form-group">
                  <label>이름</label>
-                <input type="text" name="name" class="long" disabled v-model="contact.name" ref="name" placeholder="이름을 입력하세요"/>               
+                <input type="text" name="name" class="long"  v-model="contact.name" ref="name" placeholder="이름을 입력하세요"/>               
             </div>
             <div class="form-group">
                  <label>전화번호</label>
-                <input type="text" name="tel" class="long" disabled v-model="contact.tel"  placeholder="전화번호를 입력하세요"/>               
+                <input type="text" name="tel" class="long"  v-model="contact.tel"  placeholder="전화번호를 입력하세요"/>               
             </div>            
             <div class="form-group">
                  <label>주소</label>
-                <input type="text" name="address" class="long" disabled v-model="contact.address"  placeholder="주소를 입력하세요"/>               
+                <input type="text" name="address" class="long"  v-model="contact.address"  placeholder="주소를 입력하세요"/>               
             </div>
             <div class="form-group">
                 <label>&nbsp;</label>
